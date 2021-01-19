4d4dae7b81808c28bfbb742ed2ffa78b607f1ca4
analuciachin <ananime@gmail.com>
2021-01-19 02:21:22 +0000

    add state to the app

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -21,11 +21,14 @@ export default function DayList(props) {
     },
   ];
 
-  const parsedDays = days.map(day => <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.day}
-                                                  setDay={props.setDay} />)
   return (
     <ul>
-      { parsedDays }
+      { days.map(day => <DayListItem key={day.id} 
+                          name={day.name} 
+                          spots={day.spots} 
+                          selected={day.name === props.day}
+                          setDay={props.setDay} />)
+      }
     </ul>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -38,23 +38,23 @@ storiesOf("DayListItem", module) //Initiates Storybook and registers our DayList
   ));
 
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
   
 storiesOf("DayList", module)
   .addParameters({
