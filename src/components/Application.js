47a9c69637d9059087f71ae5dd6891c12e460402
analuciachin <ananime@gmail.com>
2021-01-21 22:10:44 +0000

    work on getInterview function

--- src/components/Application.js
+++ src/components/Application.js
@@ -21,10 +21,17 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const appointments = getAppointmentsForDay(state, state.day);
+
+  const schedule = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+  })
+
 
   const setDay = day => setState({ ...state, day });
 
@@ -32,11 +39,12 @@ export default function Application(props) {
 
     Promise.all([
       axios.get("/api/days"),
-      axios.get("/api/appointments")
+      axios.get("/api/appointments"),
+      axios.get("api/interviewers")
     ])
     .then(response => {
       console.log(response);
-      setState(prev => ({ ...prev, days: response[0].data, appointments: response[1].data }))
+      setState(prev => ({ ...prev, days: response[0].data, appointments: response[1].data, interviewers: response[2].data }))
     })
   },[])
 
@@ -65,7 +73,11 @@ export default function Application(props) {
       </section>
       <section className="schedule">
           { dailyAppointments.map(appointment => 
-          <Appointment key={appointment.id} {...appointment} /> )}
+          <Appointment key={appointment.id} 
+            id={appointment.id}
+            time={appointment.time}
+            interview={interview} /> 
+          )}
           <Appointment key="last" time="5pm" />
       </section>
     </main>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,5 @@
+//import InterviewerList from "components/InterviewerList";
+
 export function getAppointmentsForDay(state, day) {
   //... returns an array of appointments for that day
   const result = [];
@@ -16,9 +18,24 @@ export function getAppointmentsForDay(state, day) {
         result.push(state.appointments[appointments[j]])
       }
     }
-    console.log(result);
+    //console.log(result);
     return result;
   }
+}
 
 
+
+export function getInterview(state, interview) {
+  let result = null;
+
+  if (!interview) {
+    return result;
+  } else {
+    result = {};
+    result.student = interview.student;
+    const interviewerObjKey = interview.interviewer;
+    console.log('here', interviewerObjKey);
+    result.interviewer = state.interviewers[interviewerObjKey]
+    return result;
+  }
 }
