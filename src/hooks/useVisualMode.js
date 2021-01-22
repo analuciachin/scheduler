8c2fb465079c629d98e452714bf0ebc1039b8bf4
analuciachin <ananime@gmail.com>
2021-01-22 03:29:03 +0000

    build a hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(mode, replace = false) {
+    setMode(mode);
+    setHistory(replace ? history : [...history, mode]);
+  }
+
+  function back() {
+    if (history.length > 1) {
+      setHistory(history.slice(0, -1))
+      setMode(history[history.length - 2])
+    } else {
+      setMode(initial);
+      setHistory(initial);
+    }
+  }
+
+  return { mode, transition, back };
+}
